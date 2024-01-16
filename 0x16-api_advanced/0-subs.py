3af0d025b7be7fb92320ffb0009c18063ead00e4
Liam Bowers <liambowers718@gmail.com>
2024-01-16 13:03:57 -0800

    Removed else block from task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,8 +2,7 @@
 """
 number of subscribers for a given subreddit
 """
-
-from requests import get
+import requests
 
 
 def number_of_subscribers(subreddit):
@@ -11,14 +10,9 @@ def number_of_subscribers(subreddit):
     function that queries the Reddit API and returns the number of subscribers
     (not active users, total subscribers) for a given subreddit.
     """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
-    user_agent = {'User-agent': 'linux:0x16.advanced:v1.0.0 (by /u/midigan'}
+    user_agent = {"User-agent": "linux:0x16.advanced:v1.0.0 (by /u/midigan)"}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent, allow_redirects=False)
-    if response.status_code != 200:
-        return 0
-    results = response.json().get('data')
-    return results.get('subscribers')
+    response = requests.get(url, headers=user_agent, allow_redirects=False)
+    if response.status_code == 200:
+        return response.json().get('data').get('subscribers')
+    return 0
