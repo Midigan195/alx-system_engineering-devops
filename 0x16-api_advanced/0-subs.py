a4d368d499098b5d373f234169cebd771a887b11
Liam Bowers <liambowers718@gmail.com>
2024-01-16 04:44:07 -0800

    Updated task 0 to check value is not a redirection

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
-import requests
+"""
+number of subscribers for a given subreddit
+"""
+
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api:v1.0.0 (by /u/midigan)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+
+    user_agent = {'User-agent': 'linux:0x16.advanced:v1.0.0 (by /u/midigan'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+    results = results.get('data')
+    if 'subscribers' in results:
+        return results.get('subscribers')
+    else:
+        return 0
